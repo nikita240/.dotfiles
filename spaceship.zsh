c8af65300adb739594a3979e14f2560d15fc93a8
Nikita Rushmanov <rush3nik@gmail.com>
2023-01-04 14:58:47 -0800

    Fix docker symbol

--- spaceship.zsh
+++ spaceship.zsh
@@ -4,9 +4,8 @@ spaceship remove docker
 spaceship add --after dir docker_shell
 
 function spaceship_docker_shell() {
-  echo "SECTION"
   if [ -f /.dockerenv ]; then
-    spaceship::section::v4 --prefix "in " "${SPACESHIP_DOCKER_SYMBOL}"
+    spaceship::section::v4 --prefix "in " "🐳 "
   fi
 }
 
